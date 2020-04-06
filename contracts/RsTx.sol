d47b008c0597e8c3b639b2c74b2b6ad71ffaf421
Rohit Narurkar <rohitnarurkar@gmail.com>
2020-04-06 21:09:21 +0530

    add parameter for encrypted recipient

--- contracts/RsTx.sol
+++ contracts/RsTx.sol
@@ -3,9 +3,13 @@ pragma solidity 0.5.11;
 
 contract RsTx {
 
-    event NewRsTx(uint256 noncePointX, uint256 noncePointY);
+    event NewRsTx(uint256 noncePointX, uint256 noncePointY, bytes encryptedRecipient);
 
-    function rsTx(uint256 noncePointX, uint256 noncePointY) external {
-        emit NewRsTx(noncePointX, noncePointY);
+    function rsTx(
+        uint256 noncePointX,
+        uint256 noncePointY,
+        bytes calldata encryptedRecipient
+    ) external {
+        emit NewRsTx(noncePointX, noncePointY, encryptedRecipient);
     }
 }
